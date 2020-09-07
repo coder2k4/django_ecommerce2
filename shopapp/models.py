329de66a5f98a0c5af4bb9b93a88a116abf0d52b
Standalone <coder2k@mail.ru>
2020-09-07 21:21:27 +0300

    Добавлены related_name Создание миграции

--- shopapp/models.py
+++ shopapp/models.py
@@ -29,7 +29,8 @@ class Product(models.Model):
 class CartProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE,
+                                related_name='related_products')
     quantity = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
@@ -39,7 +40,7 @@ class CartProduct(models.Model):
 
 class Cart(models.Model):
     owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
-    products = models.ManyToManyField(CartProduct, blank=True)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
@@ -53,7 +54,7 @@ class Customer(models.Model):
     address = models.CharField(max_length=255, verbose_name='Адрес')
 
     def __str__(self):
-        return "Опкупатель: {} {}".format(self.user.first_name, self.user.last_name)
+        return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
 
 
 class Specification(models.Model):
@@ -62,4 +63,4 @@ class Specification(models.Model):
     name = models.CharField(max_length=255, verbose_name='Имя товара для характеристик')
 
     def __str__(self):
-        return "Хакактериски для товара: {}".format(self.name)
+        return "Характеристики для товара: {}".format(self.name)
